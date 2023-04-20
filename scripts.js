36fcfe0bc7aa8236c416520ef718c949cdfa4358
andrii.prokhnitskyi <26540483+Prokhnitskyi@users.noreply.github.com>
2023-04-20 21:45:01 +0300

    Add function for basic operation

---
+++ scripts.js
@@ -0,0 +1,38 @@
+function add(a,b) {
+    return parseFloat(a) + parseFloat(b);
+}
+
+function subtract(a,b) {
+    return parseFloat(a) - parseFloat(b);
+}
+
+function multiply(a,b) {
+    return parseFloat(a) * parseFloat(b);
+}
+
+function divide(a,b) {
+    return parseFloat(a) / parseFloat(b);
+}
+
+function operate(firstNumber, secondNumber, operator) {
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = add(firstNumber, secondNumber);
+            break;
+        case '-':
+            result = subtract(firstNumber, secondNumber);
+            break;
+        case '*':
+            result = multiply(firstNumber, secondNumber);
+            break;
+        case '/':
+            result = divide(firstNumber, secondNumber);
+            break;
+        default:
+            throw new Error('Unknown operation');
+    }
+
+    return result;
+}
